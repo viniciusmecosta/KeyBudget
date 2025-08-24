fa280974af275a89da2c02048fe0a45245147696
viniciusmecosta <costavinicius315@gmail.com>
2025-08-24 12:56:02 -0300

    perf: Optimize the app for prod

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,12 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
