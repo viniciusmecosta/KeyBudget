205444105bf58107edd03ba6b1e1953eed6f617c
viniciusmecosta <costavinicius315@gmail.com>
2025-09-27 17:03:50 -0300

    feat: error snackbar

---
+++ lib/core/services/snackbar_service.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SnackbarService {
+  static void showError(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+
+  static void showSuccess(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+}
