4033c4578f27e777dae63bca436f2d3645221469
viniciusmecosta <costavinicius315@gmail.com>
2025-10-12 20:12:22 -0300

    feat: add haptic feedback

---
+++ lib/app/utils/navigation_utils.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NavigationUtils {
+  static Future<T?> push<T extends Object?>(BuildContext context, Widget page) {
+    return Navigator.of(context).push<T>(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => page,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(0.0, 1.0);
+          const end = Offset.zero;
+          const curve = Curves.easeInOutCubic;
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 300),
+      ),
+    );
+  }
+}
